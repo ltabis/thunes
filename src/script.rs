bc425a91bed29123c817bdd58092a0d3385fb4ec
ltabis <ltabis.pro@gmail.com>
2024-06-02 17:39:12 +0200

    feat: add an abstraction to sum transactions from rhai scripts

--- src/script.rs
+++ src/script.rs
@@ -16,11 +16,28 @@ pub mod env {
     }
 }
 
+#[export_module]
+pub mod prelude {
+    /// Sum the total of the given transaction array.
+    ///
+    /// rhai-autodocs:index:2
+    #[rhai_fn(return_raw)]
+    pub fn sum(transactions: &mut rhai::Array) -> Result<rhai::FLOAT, Box<rhai::EvalAltResult>> {
+        let transactions = transactions
+            .iter()
+            .map(rhai::serde::from_dynamic)
+            .collect::<Result<Vec<TransactionRhai>, _>>()?;
+
+        Ok(transactions.iter().map(|op| op.amount).sum())
+    }
+}
+
 pub fn build_engine() -> rhai::Engine {
     let mut engine = rhai::Engine::new();
 
     rhai_http::HttpPackage::new().register_into_engine(&mut engine);
     engine.register_static_module("env", rhai::exported_module!(env).into());
+    engine.register_global_module(rhai::exported_module!(prelude).into());
     engine.register_type::<TransactionRhai>();
 
     engine
