43553a38a5d61fc132d4ac3160d9629a6190e9cc
ltabis <ltabis.pro@gmail.com>
2024-12-30 17:17:13 +0100

    chore: update eslint config

--- desktop/eslint.config.js
+++ desktop/eslint.config.js
@@ -1,4 +1,3 @@
-import react from "eslint-plugin-react";
 import reactHooks from "eslint-plugin-react-hooks";
 import js from "@eslint/js";
 import tseslint from 'typescript-eslint';
@@ -11,15 +10,12 @@ export default tseslint.config(
         files: ['src/**/*.{ts,tsx}'],
 
         plugins: {
-            "react": react,
             "react-hooks": reactHooks,
         },
 
         rules: {
-            ...js.configs.recommended.rules,
-            ...react.configs.recommended.rules,
-            ...react.configs["jsx-runtime"].rules,
             ...reactHooks.configs.recommended.rules,
+            "@typescript-eslint/no-unused-vars": "warn",
         },
 
         languageOptions: {
