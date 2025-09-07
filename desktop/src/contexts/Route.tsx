662e06d61539698bab02ee590ec10dea531b5bd6
ltabis <ltabis.pro@gmail.com>
2025-09-07 15:31:10 +0200

    chore: cleanup

--- desktop/src/contexts/Route.tsx
+++
@@ -1,6 +0,0 @@
-import { createContext } from 'react';
-
-export type Route = "dashboard" | "account";
-
-export const RouteContext = createContext("dashboard" as Route);
-export const RouteDispatchContext = createContext(null);
